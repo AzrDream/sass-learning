4cade3cfd17829da7e36b093c2e53a8bc3c3973d
AzrDream <1499325862@qq.com>
2022-07-04 12:03:46 +0800

    蘑菇街正文图片中

--- 蘑菇街/js/index.js
+++ 蘑菇街/js/index.js
@@ -1,4 +1,27 @@
 window.onload = function () {
+    // 1.初始化顶部的导航
+    initNav();
+    // 2.初始化控制文字行数的代码
+    initTextRow();
+};
+function initTextRow() {
+    /*
+    // 1.拿到需要操作的元素
+    let oDes = document.querySelector(".mask-des");
+    // 2.拿到行高
+    let lineHeight = parseFloat(getComputedStyle(oDes).lineHeight);
+    // 3.拿到元素的行高
+    let height = parseFloat(getComputedStyle(oDes).height);
+    // 4.判断元素的高度是否超出了3倍的行高
+    if(height > (3 * lineHeight)) {
+        oDes.style.height = (3 * lineHeight) + "px";
+        oDes.style.overflow = "hidden";
+    }
+     */
+    let oDes = document.querySelector(".mask-des");
+    $clamp(oDes,{clamp: 2});
+}
+function initNav() {
     // 1.拿到原有的顶部的导航
     let oNav = document.querySelector(".nav");
     let oNav2 = document.querySelector(".nav2");
@@ -17,4 +40,4 @@ window.onload = function () {
             oNav2.style.top = "-100px";
         }
     }
-};
+}
